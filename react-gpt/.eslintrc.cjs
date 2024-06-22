e61998fa026bdba92c78cd9b7853a2e09b83c7c8
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-22 04:05:59 -0500

    chore: Add initial project files and configurations

---
+++ react-gpt/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'standard',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'global-require': 'off',
+    'space-before-function-paren': 'off',
+    'no-undef': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'multiline-ternary': 'off',
+  },
+}
