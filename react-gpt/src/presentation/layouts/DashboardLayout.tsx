0847feebd4525d17306f5628e11e960bb54aebd1
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-22 04:07:58 -0500

    feat: Add DashboardLayout component for React GPT project

---
+++ react-gpt/src/presentation/layouts/DashboardLayout.tsx
@@ -0,0 +1,35 @@
+import { Outlet } from 'react-router-dom'
+
+import { SidebarMenuItem } from '../components'
+import { menuRoutes } from '../router/router'
+
+export default function DashboardLayout() {
+  return (
+    <main className="flex flex-row mt-7">
+      <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-white bg-opacity-10 p-5 rounded-3xl">
+        <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 bg-clip-text text-transparent">
+          ReactGPT<span className="text-indigo-500">.</span>
+        </h1>
+        <span className="text-xl">Bienvenido</span>
+
+        <div className="border-gray-700 border my-3" />
+
+        {/* Opciones del menú */}
+        {menuRoutes.map((option) => (
+          <SidebarMenuItem
+            key={option.to}
+            option={option}
+          />
+        ))}
+      </nav>
+
+      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)]  bg-white bg-opacity-10 p-5 rounded-3xl">
+        <div className="flex flex-row h-full">
+          <div className="flex flex-col flex-auto h-full p-1">
+            <Outlet />
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
