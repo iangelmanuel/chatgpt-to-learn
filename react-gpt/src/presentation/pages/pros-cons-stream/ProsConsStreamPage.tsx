53a6c9a3c0a9b883ce011e179df2733467c28de7
iangelmanuel <iangelmanueldm@gmail.com>
2024-06-25 04:00:50 -0500

    feat: Add lazy loading for images in ProsConsStreamPage and ProsConsPage
    This commit adds lazy loading functionality for images in the ProsConsStreamPage and ProsConsPage components. By implementing lazy loading, the page load time is improved as images are loaded only when they are visible in the viewport. This enhances the user experience by reducing the initial load time and improving overall performance.

--- react-gpt/src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ react-gpt/src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,5 +1,114 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+
+import { prosConsGeneratorUseCase } from '../../../core/use-cases'
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader
+} from '../../components'
+
+type Message = {
+  text: string
+  isGpt: boolean
+}
 
 export const ProsConsStreamPage = () => {
-  return <div>PropsConsStreamPage</div>
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+  const abortController = useRef(new AbortController())
+  const isRunning = useRef(false)
+
+  const handlePost = async (message: string) => {
+    if (isRunning.current) {
+      abortController.current.abort()
+      abortController.current = new AbortController()
+    }
+
+    abortController.current.abort()
+    setIsLoading(true)
+    isRunning.current = true
+    setMessages((prevState) => [...prevState, { text: message, isGpt: false }])
+
+    // useCase
+    const stream = prosConsGeneratorUseCase(
+      message,
+      abortController.current.signal
+    )
+    setIsLoading(false)
+
+    setMessages((prevState) => [...prevState, { text: '', isGpt: true }])
+
+    for await (const value of stream) {
+      setMessages((prevState) => {
+        const newMessages = [...prevState]
+        newMessages[newMessages.length - 1].text = value
+        return newMessages
+      })
+    }
+
+    isRunning.current = false
+
+    // const reader = await prosConsDiscusserStreamUseCase(message)
+    // setIsLoading(false)
+
+    // // Generar el ultimo mensaje
+    // if (!reader) return alert('No se pudo obtener la respuesta del servidor.')
+
+    // const decoder = new TextDecoder()
+    // let messageData = ''
+    // setMessages((prevState) => [
+    //   ...prevState,
+    //   { text: messageData, isGpt: true }
+    // ])
+
+    // while (true) {
+    //   const { value, done } = await reader.read()
+
+    //   if (done) break
+
+    //   const decodedChunk = decoder.decode(value, { stream: true })
+    //   // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+    //   messageData += decodedChunk
+
+    //   // Generar el mensaje
+    //   setMessages((prevState) => {
+    //     const newMessages = [...prevState]
+    //     newMessages[newMessages.length - 1].text = messageData
+    //     return newMessages
+    //   })
+    // }
+  }
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage
+                key={index}
+                text={message.text}
+              />
+            ) : (
+              <MyMessage
+                key={index}
+                text={message.text}
+              />
+            )
+          )}
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader className="fade-in" />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe un mensaje..."
+        disableCorrections
+      />
+    </div>
+  )
 }
--- react-gpt/src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ react-gpt/src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,5 +1,75 @@
-import React from 'react'
+import { useState } from 'react'
+
+import { prosConsDiscusserUseCase } from '../../../core/use-cases'
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader
+} from '../../components'
+
+type Message = {
+  text: string
+  isGpt: boolean
+}
 
 export const ProsConsPage = () => {
-  return <div>ProsConsPage</div>
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async (message: string) => {
+    setIsLoading(true)
+    setMessages((prevState) => [...prevState, { text: message, isGpt: false }])
+
+    // useCase
+    const data = await prosConsDiscusserUseCase(message)
+
+    if (!data.ok) {
+      setMessages((prevState) => [
+        ...prevState,
+        { text: data.content, isGpt: true }
+      ])
+    } else {
+      setMessages((prevState) => [
+        ...prevState,
+        { text: data.content, isGpt: true }
+      ])
+    }
+
+    setIsLoading(false)
+
+    // Añadir el mensaje de isGPT en true
+  }
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage
+                key={index}
+                text={message.text}
+              />
+            ) : (
+              <MyMessage
+                key={index}
+                text={message.text}
+              />
+            )
+          )}
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader className="fade-in" />
+            </div>
+          )}
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe un mensaje..."
+        disableCorrections
+      />
+    </div>
+  )
 }
