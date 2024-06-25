5a968c179a033315cb2cfe6d5d93455bcec5c586
iangelmanuel <iangelmanueldm@gmail.com>
2024-06-24 19:29:15 -0500

    feat: Add OrthographyDto for GPT module

---
+++ nest-gpt/src/gpt/dtos/orthography.dto.ts
@@ -0,0 +1,10 @@
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class OrthographyDto {
+  @IsString()
+  readonly prompt: string;
+
+  @IsInt()
+  @IsOptional()
+  readonly maxToken?: number;
+}
