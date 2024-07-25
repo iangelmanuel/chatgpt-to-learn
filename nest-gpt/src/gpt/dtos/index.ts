98b7a919db26d41808d9dab1fc376d35c7b119f9
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-25 03:13:00 -0500

    feat: Add audio-to-text DTO and update DTO index

--- nest-gpt/src/gpt/dtos/index.ts
+++ nest-gpt/src/gpt/dtos/index.ts
@@ -2,3 +2,4 @@ export * from './orthography.dto';
 export * from './pros-const-discusser.dto';
 export * from './translate.dto';
 export * from './text-to-audio.dto';
+export * from './audio-to-text.dto';
