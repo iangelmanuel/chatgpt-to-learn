5dc8986efd34d62f57fad17275b7010b6e0ea7c2
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-25 03:52:43 -0500

    feat: Add audio-to-text-response type definition

---
+++ react-gpt/src/types/audio-to-text-response.ts
@@ -0,0 +1,20 @@
+export interface Segment {
+  id: number
+  seek: number
+  start: number
+  end: number
+  text: string
+  tokens: number[]
+  temperature: number
+  avg_logprob: number
+  compression_ratio: number
+  no_speech_prob: number
+}
+
+export interface AudioToTextResponse {
+  task: string
+  language: string
+  duration: number
+  text: string
+  segments: Segment[]
+}
