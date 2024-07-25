a97e1f9cce75262b915813569bfd7f35d6b36784
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-25 07:58:22 -0500

    feat: Add image generation and variation use cases

---
+++ react-gpt/src/core/use-cases/image-variation.use-case.ts
@@ -0,0 +1,31 @@
+interface Image {
+  url: string
+  alt: string
+}
+type GeneratedImage = Image | null
+
+export const imageVariationUseCase = async (
+  originalImage: string
+): Promise<GeneratedImage> => {
+  try {
+    const resp = await fetch(
+      `${import.meta.env.VITE_GPT_API}/image-variation`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          baseImage: originalImage
+        })
+      }
+    )
+
+    const { url, revised_prompt: alt } = await resp.json()
+
+    return { url, alt }
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
